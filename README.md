60a3bbab66dd81a77bd5030ce363ca05136bf35a
statisthong <110047840+statisthong@users.noreply.github.com>
2023-12-08 20:06:23 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+#  watsonx.ai 와 Streamlit 을 활용한 앱 만들기
+이전 세션을 통해 watsonx에 대해 기본적으로 이해할 수 있었습니다. 이제 Watson을 사용하여 자신만의 LLM 앱을 만들 수 있습니다. 이 튜토리얼에서는 Watson Machine Learning 라이브러리로 LLM 앱 구동을 위해 Streamlit을 직접 구성하는 방법에 대해 배울 것입니다.
+
+# 실행 방법 🚀
+1. 자신의 컴퓨터 터미널 혹은 콘솔을 시작하세요
+2. 가상환경안에 있는지 확인하세요
+3. 우리가 실행할 코드 (Lab 5 - watsonxai_demo_kor_streamlit)로 이동하세요
+4. .env 파일을 해당 폴더 안에 복사하세요
+5. 다음 명령어를 입력하여 앱을 사용 해보세요 `streamlit run app.py`
+
+
+
+👨🏾‍💻 Author: Nick Renotte
---
+++ app.py
@@ -0,0 +1,69 @@
+
+# Import environment loading library
+from dotenv import load_dotenv
+# Import IBMGen Library 
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from langchain.llms.base import LLM
+# Import lang Chain Interface object
+from langChainInterface import LangChainInterface
+# Import system libraries
+import os
+# Import streamlit for the UI 
+import streamlit as st
+import re
+# import qa-model
+from qa_model import *
+
+# Load environment vars
+load_dotenv()
+
+# Define credentials 
+api_key = os.getenv("API_KEY", None)
+ibm_cloud_url = os.getenv("IBM_CLOUD_URL", None)
+project_id = os.getenv("PROJECT_ID", None)
+
+
+if api_key is None or ibm_cloud_url is None or project_id is None:
+    print("Ensure you copied the .env file that you created earlier into the same directory as this notebook")
+else:
+    creds = {
+        "url": ibm_cloud_url,
+        "apikey": api_key 
+    }
+
+# Define generation parameters 
+params = {
+    GenParams.DECODING_METHOD: "greedy",
+    GenParams.MIN_NEW_TOKENS: 1,
+    GenParams.MAX_NEW_TOKENS: 600,
+    GenParams.TEMPERATURE: 0,
+    # GenParams.TOP_K: 100,
+    # GenParams.TOP_P: 1,
+    GenParams.REPETITION_PENALTY: 1
+    #GenParams.STOP_SEQUENCES: ["Pertanyaan"]
+}
+
+
+granite_chat ="ibm/granite-13b-chat-v1"
+flanul= "google/flan-ul2"
+llama2= "meta-llama/llama-2-70b-chat"
+mt_zero = "bigscience/mt0-xxl-13b"
+
+llm = LangChainInterface(model=llama2, credentials=creds, params=params, project_id=project_id)
+
+# Title for the app
+st.title('🤖 질의 응답을 위한 AI')
+
+# Prompt box
+prompts = st.text_input('질문을 입력하세요')
+
+# Submit button
+if st.button('Submit'):
+    if prompts:
+        # Show loading spinner
+        with st.spinner('처리중...'):
+            answer_generations = answer_retrieval(llm, question_prompt(user_question=prompts, qa_sample=question_answer_id))
+            st.write(answer_generations)
+    else:
+        st.warning('프롬프트를 입력 해주세요.')
+
